ae0d9539cf0a43164abdad2ae0417f1917f8c7cc
Foo JH <foojihaw@hotmail.com>
2019-08-29 00:02:29 -0500

    Ecosystem first cut

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name: "pingsvc",
+    script: "./src/server.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }],
+  deploy: {
+    production: {
+      user: 'root',
+      host: ['localhost'],
+      repo: 'git@github.com:jhfoo/pingsvc.git',
+      ref: 'origin/master',
+      repo: 'git@github.com:jhfoo/pingsvc.git',
+      path: '/root/prod/pingsvc',
+      'post-deploy': 'npm install'
+    }
+  }
+}
